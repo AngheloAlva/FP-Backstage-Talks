41185378739d8d9b598beeca0e248271bec0df41
Anghelo Alva <ance.anghelo@gmail.com>
2023-08-20 16:02:52 -0400

    add issues component style

--- components/Issue.jsx
+++ components/Issue.jsx
@@ -1,3 +1,5 @@
+import './Issue.css'
+
 const Issue = ({
   img,
   title,
@@ -9,21 +11,23 @@ const Issue = ({
   return (
     <div className='issue-container'>
       <img src={img} alt={title} className="issue-img" />
-      <h3>{title}</h3>
-      <p>
-        <a href='#'>{buyLink[0]} </a>
-        {buyLink[1]}
-      </p>
-      <p>
-        <a href="#">{buyLink2[0]} </a>
-        {buyLink2[1]}
-      </p>
-      {
-        buyText && <p>or in <a href="#">selected stores</a>.</p>
-      }
-      {
-        desc && <p>If you ar lucky, you may get the last pieces in <a href="#">selected stores</a>.</p>
-      }
+      <div className='issue-text'>
+        <h3>{title}</h3>
+        <p>
+          <a href='#'>{buyLink[0]} </a>
+          {buyLink[1]}
+        </p>
+        <p>
+          <a href="#">{buyLink2[0]} </a>
+          {buyLink2[1]}
+        </p>
+        {
+          buyText && <p style={{ marginTop: '.938rem' }}>or in <a href="#">selected stores</a>.</p>
+        }
+        {
+          desc && <p>If you ar lucky, you may get the last pieces in <a href="#">selected stores</a>.</p>
+        }
+      </div>
     </div>
   )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -16,48 +16,53 @@ function App () {
 
   return (
     <>
-      <h1>BACKSTAGE TALKS</h1>
-
-      <Issue
-        img={imgLinks[0]}
-        title={'Issue #7'}
-        buyLink={['BUY HERE', '(Europe)']}
-        buyLink2={['BUY HERE', '(UK & Overseas)']}
-        buyText
-      />
-      <Issue
-        img={imgLinks[1]}
-        title={'Issue #6'}
-        buyLink={['BUY HERE']}
-        buyText
-      />
-      <Issue
-        img={imgLinks[2]}
-        title={'Issue #5'}
-        buyLink={['BUY HERE']}
-        buyText
-      />
-      <Issue
-        img={imgLinks[3]}
-        title={'Issue #4 is sold out.'}
-        desc
-      />
-      <Issue
-        img={imgLinks[4]}
-        title={'Issue #3'}
-        buyText
-      />
-      <Issue
-        img={imgLinks[5]}
-        title={'Issue #2'}
-        buyText
-      />
-      <Issue
-        img={imgLinks[6]}
-        title={'Issue #1 is sold out.'}
-        desc
-        buyText
-      />
+      <header>
+        <img src='../src/assets/logo.png' alt= 'logo' className='logo-img' />
+      </header>
+      <main>
+        <Issue
+          img={imgLinks[0]}
+          title={'Issue #7'}
+          buyLink={['BUY HERE', '(Europe)']}
+          buyLink2={['BUY HERE', '(UK & Overseas)']}
+          buyText
+          bgColor={'var(--orange)'}
+        />
+        <Issue
+          img={imgLinks[1]}
+          title={'Issue #6'}
+          buyLink={['BUY HERE']}
+          buyText
+        />
+        <Issue
+          img={imgLinks[2]}
+          title={'Issue #5'}
+          buyLink={['BUY HERE']}
+          buyText
+        />
+        <Issue
+          img={imgLinks[3]}
+          title={'Issue #4 is sold out.'}
+          desc
+        />
+        <Issue
+          img={imgLinks[4]}
+          title={'Issue #3'}
+          buyLink={['BUY HERE']}
+          buyText
+        />
+        <Issue
+          img={imgLinks[5]}
+          title={'Issue #2'}
+          buyLink={['BUY HERE']}
+          buyText
+        />
+        <Issue
+          img={imgLinks[6]}
+          title={'Issue #1 is sold out.'}
+          desc
+        />
+      </main>
       <Footer />
     </>
   )
