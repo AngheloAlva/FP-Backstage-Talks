da9372759900f0894da852103f7f68bfa8b75097
Anghelo Alva <ance.anghelo@gmail.com>
2023-08-20 16:27:15 -0400

    add footer style

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,15 +1,17 @@
+import './Footer.css'
+
 const Footer = () => {
   return (
     <footer>
       <p>
-        Backstage Talks is a magazine of casual, but in depth dialogues on design and business. Our decisions shape and influence this complex world--to have a chance to make the roght ones, we need to talk.
+        Backstage Talks is a magazine of casual, but in depth dialogues on design and business. Our decisions shape and influence this complex world--to have a chance to make the right ones, we need to talk.
       </p>
-      <p>
+      <p className='footer__copyright'>
         © 2023 <a href="#">Published by Büro Milk</a>
       </p>
 
-      <h3><a href="#">Privacy Policy</a></h3>
-      <h3><a href="#">info@backstagestalks.com</a></h3>
+      <h3 className='footer__policy'><a href="#">Privacy Policy</a></h3>
+      <h3 className='footer__email'><a href="#">info@backstagestalks.com</a></h3>
     </footer>
   )
 }
