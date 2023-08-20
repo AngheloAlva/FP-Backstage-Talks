2c5926098a5ffd7c43395d7025be36a87f48f250
Anghelo Alva <ance.anghelo@gmail.com>
2023-08-20 19:27:44 -0400

    add responsive style

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,15 +3,29 @@ import './Footer.css'
 const Footer = () => {
   return (
     <footer>
-      <p>
+      <p className='footer__desc'>
         Backstage Talks is a magazine of casual, but in depth dialogues on design and business. Our decisions shape and influence this complex world--to have a chance to make the right ones, we need to talk.
       </p>
       <p className='footer__copyright'>
         © 2023 <a href="#">Published by Büro Milk</a>
       </p>
 
-      <h3 className='footer__policy'><a href="#">Privacy Policy</a></h3>
-      <h3 className='footer__email'><a href="#">info@backstagestalks.com</a></h3>
+      <div className='footer__links'>
+        <h3 className='footer__policy'><a href="#">Privacy Policy</a></h3>
+          <h3 className='footer__email'><a href="#">info@backstagestalks.com</a></h3>
+      </div>
+
+      <div className='footer__issues'>
+        <ul>
+          <li>Issue #7</li>
+          <li>Issue #6</li>
+          <li>Issue #5</li>
+          <li>Issue #4</li>
+          <li>Issue #3</li>
+          <li>Issue #2</li>
+          <li>Issue #1</li>
+        </ul>
+      </div>
     </footer>
   )
 }
