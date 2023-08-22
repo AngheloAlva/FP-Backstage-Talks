b589b6a968cded48fa640ed27c0fbe8b7230e70b
Anghelo Alva <ance.anghelo@gmail.com>
2023-08-22 15:29:59 -0400

    add scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -29,16 +29,16 @@ function App () {
 
     if (scrollPosition > issue6Ref.current.offsetTop) {
       setBgColor('var(--red)')
-    } else if (scrollPosition > issue5Ref.current.offsetTop + issue5Ref.current.offsetHeight / 1.7) {
+    } else if (scrollPosition > issue5Ref.current.offsetTop + issue5Ref.current.offsetHeight / 1.8) {
       setBgColor('var(--blue)')
-    } else if (scrollPosition > issue4Ref.current.offsetTop + issue4Ref.current.offsetHeight / 1.7) {
+    } else if (scrollPosition > issue4Ref.current.offsetTop + issue4Ref.current.offsetHeight / 1.8) {
       setBgColor('var(--yellow)')
-    } else if (scrollPosition > issue3Ref.current.offsetTop + issue3Ref.current.offsetHeight / 1.7) {
+    } else if (scrollPosition > issue3Ref.current.offsetTop + issue3Ref.current.offsetHeight / 1.8) {
       setBgColor('var(--orange)')
-    } else if (scrollPosition > issue2Ref.current.offsetTop + issue2Ref.current.offsetHeight / 1.7) {
+    } else if (scrollPosition > issue2Ref.current.offsetTop + issue2Ref.current.offsetHeight / 1.8) {
       setBgColor('var(--sky-blue)')
       setFirstLinkColor('#FFFFFF')
-    } else if (scrollPosition > issue1Ref.current.offsetTop + issue1Ref.current.offsetHeight / 1.7) {
+    } else if (scrollPosition > issue1Ref.current.offsetTop + issue1Ref.current.offsetHeight / 1.8) {
       setBgColor('var(--white)')
       setFirstLinkColor('var(--pink)')
     } else {
@@ -50,6 +50,103 @@ function App () {
   useEffect(() => {
     window.addEventListener('scroll', handleBgColor)
     return () => window.removeEventListener('scroll', handleBgColor)
+  }, [])
+
+  const nextIssue = () => {
+    const scrollPosition = window.pageYOffset
+
+    if (scrollPosition < issue1Ref.current.offsetTop) {
+      window.scrollTo({
+        top: issue1Ref.current.offsetTop,
+        behavior: 'smooth'
+      })
+    } else if (scrollPosition < issue2Ref.current.offsetTop) {
+      window.scrollTo({
+        top: issue2Ref.current.offsetTop,
+        behavior: 'smooth'
+      })
+    } else if (scrollPosition < issue3Ref.current.offsetTop) {
+      window.scrollTo({
+        top: issue3Ref.current.offsetTop,
+        behavior: 'smooth'
+      })
+    } else if (scrollPosition < issue4Ref.current.offsetTop) {
+      window.scrollTo({
+        top: issue4Ref.current.offsetTop,
+        behavior: 'smooth'
+      })
+    } else if (scrollPosition < issue5Ref.current.offsetTop) {
+      window.scrollTo({
+        top: issue5Ref.current.offsetTop,
+        behavior: 'smooth'
+      })
+    } else if (scrollPosition < issue6Ref.current.offsetTop) {
+      window.scrollTo({
+        top: issue6Ref.current.offsetTop,
+        behavior: 'smooth'
+      })
+    } else if (scrollPosition < issue7Ref.current.offsetTop) {
+      window.scrollTo({
+        top: issue7Ref.current.offsetTop,
+        behavior: 'smooth'
+      })
+    }
+  }
+
+  const prevIssue = () => {
+    const scrollPosition = window.pageYOffset
+
+    if (scrollPosition > issue7Ref.current.offsetTop) {
+      window.scrollTo({
+        top: issue7Ref.current.offsetTop,
+        behavior: 'smooth'
+      })
+    } else if (scrollPosition > issue6Ref.current.offsetTop) {
+      window.scrollTo({
+        top: issue6Ref.current.offsetTop,
+        behavior: 'smooth'
+      })
+    } else if (scrollPosition > issue5Ref.current.offsetTop) {
+      window.scrollTo({
+        top: issue5Ref.current.offsetTop,
+        behavior: 'smooth'
+      })
+    } else if (scrollPosition > issue4Ref.current.offsetTop) {
+      window.scrollTo({
+        top: issue4Ref.current.offsetTop,
+        behavior: 'smooth'
+      })
+    } else if (scrollPosition > issue3Ref.current.offsetTop) {
+      window.scrollTo({
+        top: issue3Ref.current.offsetTop,
+        behavior: 'smooth'
+      })
+    } else if (scrollPosition > issue2Ref.current.offsetTop) {
+      window.scrollTo({
+        top: issue2Ref.current.offsetTop,
+        behavior: 'smooth'
+      })
+    } else if (scrollPosition > issue1Ref.current.offsetTop) {
+      window.scrollTo({
+        top: issue1Ref.current.offsetTop,
+        behavior: 'smooth'
+      })
+    }
+  }
+
+  const handleScroll = () => {
+    if (window.innerWidth > 992) {
+      if (window.event.deltaY > 0) {
+        nextIssue()
+      } else {
+        prevIssue()
+      }
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('wheel', handleScroll)
+    return () => window.removeEventListener('wheel', handleScroll)
   }, [])
 
   return (
